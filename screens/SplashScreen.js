f0d99702f9b4f71cdc5926c752d256332784780f
ksergenozel <ksergenozel@gmail.com>
2021-08-28 20:35:04 +0300

    iOS splash screen has been updated

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Appearance, Dimensions, Image, ImageBackground } from 'react-native';
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Appearance,
+  Dimensions,
+  Image,
+  ImageBackground,
+} from 'react-native';
 
-const { height, width } = Dimensions.get('window');
+const {height, width} = Dimensions.get('window');
 
 const Splash = () => {
-
   return (
-    <ImageBackground style={styles.main} source={require(`../assets/images/splash/splash_bg.png`)}>
-      <Image style={styles.image} source={require(`../assets/images/splash/splash_logo.png`)} />
+    <ImageBackground
+      style={styles.main}
+      source={require(`../assets/images/splash/splash_bg.png`)}>
+      <Image
+        style={styles.image}
+        source={require(`../assets/images/splash/splash_logo.png`)}
+      />
     </ImageBackground>
   );
-}
+};
 
 export default Splash;
 
-const styles =
-  StyleSheet.create({
-    main: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center"
-    },
-    image: {
-      width: width * 500 / 2160,
-      resizeMode: "contain"
-    }
-  });
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    resizeMode: 'contain',
+  },
+});
